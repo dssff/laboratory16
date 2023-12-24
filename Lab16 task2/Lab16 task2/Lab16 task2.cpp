80ab5615017e2edcd1c8106165d65ed5c2bb6d27
dssff <dendssff@gmail.com>
2023-12-25 00:47:09 +0200

    main

---
+++ Lab16 task2/Lab16 task2/Lab16 task2.cpp
@@ -0,0 +1,28 @@
+﻿#include <stdio.h>
+
+
+
+int main() {
+    // Ініціалізація матриці
+    int matrix[4][4] = {
+        {0, -65, -32, -12},
+        {56, 6, 6, 55},
+        {-33, -11, 141, -162},
+        {4, -7, 12, 556}
+    };
+
+
+    for (int i = 0; i < 4; i++) { //проходимо по кожному рядку
+        int positive = 0;
+
+        for (int j = 0; j < 4; j++) { //проходимо по кожному стовпцю в рядку який зараз обробляє цикл
+            if (matrix[i][j] > 0) {
+                positive = positive + 1;
+            }
+        }
+
+        printf("There are %d positive elements in row %d\n", i + 1, positive);
+    }
+
+    return 0;
+}
---
+++ Лаб16 з4/Лаб16 з4/Лаб16 з4.cpp
@@ -0,0 +1,53 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    // Введеня розмірів масиву MxN
+    int M, N;
+    printf("enter the number of rows (M): ");
+    scanf_s("%d", &M);
+    printf("enter the number of columns (N): ");
+    scanf_s("%d", &N);
+
+    // Створення динамічного масиву та виділення памяті
+    int** matrix = (int**)malloc(M * sizeof(int*));
+    for (int i = 0; i < M; i++) {
+        matrix[i] = (int*)malloc(N * sizeof(int));
+    }
+
+    // Заповнення матриці
+    int
+        value = 0;
+    for (int i = 0; i < M; i++) {
+        if (i % 2 == 0) {
+            // якщо парний рядок то рухаємо вправо
+            for (int j = 0; j < N; j++) {
+                matrix[i][j] = value++;
+            }
+        }
+        else {
+            // якщо непарний то рухаємось вліво, 
+            // задаємо значення  int j = N - 1 щоб перший заповненим елементом був самий правий елемент рядку
+            for (int j = N - 1; j >= 0; j--) {
+                matrix[i][j] = value++;
+            }
+        }
+    }
+
+    // Виведення матриці
+    printf("matrix filling:\n");
+    for (int i = 0; i < M; i++) {
+        for (int j = 0; j < N; j++) {
+            printf("%3d ", matrix[i][j]);
+        }
+        printf("\n");
+    }
+
+    // Звільнюємо память
+    for (int i = 0; i < M; i++) {
+        free(matrix[i]);
+    }
+    free(matrix);
+
+    return 0;
+}
